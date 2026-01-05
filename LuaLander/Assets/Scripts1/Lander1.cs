52970a2e0fbd0833d9415cf048c9c52eafcc6ea9
chrisbuczek <krzysztofbuczek@live.com>
2026-01-05 19:04:33 +0100

    LuaLander add Lander1

---
+++ LuaLander/Assets/Scripts1/Lander1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Lander1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        Debug.Log("Start");        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
---
+++ LuaLander/StructureGuide.md
@@ -0,0 +1,1 @@
+1. Always separate logic from the visuals.
