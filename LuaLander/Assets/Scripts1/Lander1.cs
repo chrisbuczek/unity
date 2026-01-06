a58d8fb64bbda7b8b7995caec71c8a9a130bf0c8
chrisbuczek <krzysztofbuczek@live.com>
2026-01-06 22:12:03 +0100

    LuaLander lander script movement

--- LuaLander/Assets/Scripts1/Lander1.cs
+++ LuaLander/Assets/Scripts1/Lander1.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Lander1 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D landerRigidbody2D;
+
+    private void Awake()
+    {
+        landerRigidbody2D = GetComponent<Rigidbody2D>(); //get reference to Rigidbody2D from the same game object
+        // Application.targetFrameRate = 10; // test physics with desired framerate
+    }
+
     private void Start()
     {
         Debug.Log("Start");        
     }
 
-    // Update is called once per frame
-    private void Update()
+    // this runs on fixed interval regardless of framerate = good for physics 
+    private void FixedUpdate()
     {
-        
+        if(Keyboard.current.upArrowKey.isPressed)
+        {
+            // transform.up is object up with local transformations: rotation, position. New Vector2D(0, 1) will give global vector
+            // Time.fixedDeltaTime makes super sure we are applying same force on every update. It's not necesary here, but I keep it.
+            // You shouldn't use Time.deltaTime inside FixedUpdate, because it defeats the purpose of FixedUpdate.
+            landerRigidbody2D.AddForce(transform.up);
+        }
+        if(Keyboard.current.rightArrowKey.isPressed)
+        {
+            Debug.Log("Right");
+        }
+        if(Keyboard.current.leftArrowKey.isPressed)
+        {
+            Debug.Log("Left");
+        }
     }
 }
--- LuaLander/StructureGuide.md
+++ LuaLander/StructureGuide.md
@@ -1,1 +1,3 @@
 1. Always separate logic from the visuals.
+2. Grab references from the same game object in Awake() function. For accessing external references from different objects use Start() function.
+3. You should generally use Update() for listening for key inputs and FixedUpdate() for physics.
