7da3c244384f5f613bf72cc9ebc48d68c4509d3e
chrisbuczek <krzysztofbuczek@live.com>
2026-08-11 21:58:28 +0200

    BrickBraker: add cinemachine, create destroyble Brick, create DeathTriggerZone

--- BricksBreaker/Assets/Scripts/Ball.cs
+++ BricksBreaker/Assets/Scripts/Ball.cs
@@ -1,12 +1,21 @@
+using System;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private Vector2 startDirection = new Vector2(1f, 1f); //45 degree angle
 
     private Rigidbody2D ballRigidBody;
     private Vector2 velocityBeforeCollision;
+
+    public event EventHandler OnDeathTriggerEntered;
+    public event EventHandler<BrickDestroyedEventArgs> OnBrickDestroyed;
+
+    public class BrickDestroyedEventArgs : EventArgs
+    {
+        public int points;
+    }
 
     private void Awake()
     {
@@ -25,20 +34,33 @@ public class Ball : MonoBehaviour
         velocityBeforeCollision = ballRigidBody.linearVelocity;
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnCollisionEnter2D(Collision2D collider2D) {
         // No contact points means no surface normal to reflect off, so there is nothing to do.
         // Happens with trigger-like overlaps and collisions the solver already pushed apart.
-        if (other.contactCount == 0) return;
+        if (collider2D.contactCount == 0) return;
 
         // Average the contacts so corners give a sane normal.
         Vector2 normal = Vector2.zero;
-        for (int i = 0; i < other.contactCount; i++)
-            normal += other.GetContact(i).normal;
+        for (int i = 0; i < collider2D.contactCount; i++)
+            normal += collider2D.GetContact(i).normal;
         normal.Normalize();
 
         Vector2 incoming = velocityBeforeCollision.normalized;
         Vector2 reflected = Vector2.Reflect(incoming, normal);
 
         ballRigidBody.linearVelocity = reflected * moveSpeed;
+
+        if(collider2D.gameObject.TryGetComponent(out Brick brick)) {
+            OnBrickDestroyed?.Invoke(this, new BrickDestroyedEventArgs { points = brick.GetPoints() });
+            brick.DestroySelf();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if(collider2D.gameObject.TryGetComponent(out DeathTriggerZone deathTriggerZone)) {
+            OnDeathTriggerEntered.Invoke(this, EventArgs.Empty);
+        }       
+        
     }
 }
--- BricksBreaker/Assets/Scripts/GameManager.cs
+++ BricksBreaker/Assets/Scripts/GameManager.cs
@@ -1,14 +1,39 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance {get; private set;}
 
+    [SerializeField] Ball ball;
+    [SerializeField] DeathTriggerZone deathTriggerZone;
+
     private static int score = 0;
+    private static bool isGameOver = false;
 
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Start()
+    {
+        ball.OnBrickDestroyed += Ball_OnBrickDestroyed;
+        ball.OnDeathTriggerEntered += Ball_OnDeathTriggerEntered;
+    }
+
+    private void Ball_OnDeathTriggerEntered(object sender, EventArgs e)
+    {
+        isGameOver = true;
+        Debug.Log("GAME OVER!!!");
+        Time.timeScale = 0f;
+        //TODO: Move to GameOver scene
+    }
+
+    private void Ball_OnBrickDestroyed(object sender, Ball.BrickDestroyedEventArgs e)
+    {
+        AddScore(e.points);
+        Debug.Log("points add " + score.ToString());
     }
 
     public int GetScore()
